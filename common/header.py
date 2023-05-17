e283fb971124b6a380be940a8af934b9da13b49d
fxKatsumi <takahata@forex.co.jp>
2023-05-17 15:41:26 +0900

    改修

--- common/header.py
+++ common/header.py
@@ -1,4 +1,58 @@
-# プログラムID
+# 顔検知プログラムID
 Face_ID_face = 1 # 01_face.py（顔検知）
 Face_ID_faceid = 4 # 04_faceid.py（顔認識）
 Face_ID_faceid_reg = 5 # 05_faceid_reg.py（顔認識（登録モード））
+
+# 物体検知プログラムID
+Object_ID_object = 2 # 02_object.py（物体検知）
+Object_ID_human = 3 # 03_human.py（人物検知）
+
+# クラス名（英語）
+CLASSES_E = [
+    "background",
+    "aeroplane",
+    "bicycle",
+    "bird",
+    "boat",
+    "bottle",
+    "bus",
+    "car",
+    "cat",
+    "chair",
+    "cow",
+    "diningtable",
+    "dog",
+    "horse",
+    "motorbike",
+    "person",
+    "pottedplant",
+    "sheep",
+    "sofa",
+    "train",
+    "tvmonitor",
+]
+
+# クラス名（日本語）
+CLASSES_J = [
+    "背景",
+    "飛行機",
+    "自転車",
+    "鳥",
+    "船",
+    "ボトル",
+    "バス",
+    "車",
+    "猫",
+    "椅子",
+    "牛",
+    "テーブル",
+    "犬",
+    "馬",
+    "バイク",
+    "人間",
+    "植木",
+    "羊",
+    "ソファー",
+    "列車",
+    "モニター",
+]
--- pages/02_object.py
+++ pages/02_object.py
@@ -1,114 +1,117 @@
-import streamlit as st
-import cv2
-from PIL import Image
-import numpy as np
-from streamlit_webrtc import webrtc_streamer
-import av
-from pathlib import Path
+# import streamlit as st
+# import cv2
+# from PIL import Image
+# import numpy as np
+# from streamlit_webrtc import webrtc_streamer
+# import av
+# from pathlib import Path
 
-from const import CLASSES, COLORS
-from settings import DEFAULT_CONFIDENCE_THRESHOLD, DEMO_IMAGE, MODEL, PROTOTXT
+# from const import CLASSES, COLORS
+# from settings import DEFAULT_CONFIDENCE_THRESHOLD, DEMO_IMAGE, MODEL, PROTOTXT
 
-# ダウンロードモジュール
-from sample_utils.download import download_file
+# # ダウンロードモジュール
+# from sample_utils.download import download_file
 
-HERE = Path(__file__).parent
-ROOT = HERE.parent
+from common.header import Object_ID_object
+from common.objectmain import appmain
 
-MODEL_URL = "https://github.com/robmarkcole/object-detection-app/raw/master/model/MobileNetSSD_deploy.caffemodel"  # noqa: E501
-MODEL_LOCAL_PATH = ROOT / "./model/MobileNetSSD_deploy.caffemodel"
-PROTOTXT_URL = "https://github.com/robmarkcole/object-detection-app/raw/master/model/MobileNetSSD_deploy.prototxt.txt"  # noqa: E501
-PROTOTXT_LOCAL_PATH = ROOT / "./model/MobileNetSSD_deploy.prototxt.txt"
+# HERE = Path(__file__).parent
+# ROOT = HERE.parent
 
-@st.cache
-def process_image(image):
-    blob = cv2.dnn.blobFromImage(
-        cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5
-    )
-    net = cv2.dnn.readNetFromCaffe(PROTOTXT, MODEL)
-    net.setInput(blob)
-    detections = net.forward()
-    return detections
+# MODEL_URL = "https://github.com/robmarkcole/object-detection-app/raw/master/model/MobileNetSSD_deploy.caffemodel"  # noqa: E501
+# MODEL_LOCAL_PATH = ROOT / "./model/MobileNetSSD_deploy.caffemodel"
+# PROTOTXT_URL = "https://github.com/robmarkcole/object-detection-app/raw/master/model/MobileNetSSD_deploy.prototxt.txt"  # noqa: E501
+# PROTOTXT_LOCAL_PATH = ROOT / "./model/MobileNetSSD_deploy.prototxt.txt"
 
-@st.cache
-def annotate_image(
-    image, detections, confidence_threshold=DEFAULT_CONFIDENCE_THRESHOLD
-):
-    # loop over the detections
-    (h, w) = image.shape[:2]
-    labels = []
-    for i in np.arange(0, detections.shape[2]):
-        confidence = detections[0, 0, i, 2]
+# @st.cache
+# def process_image(image):
+#     blob = cv2.dnn.blobFromImage(
+#         cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5
+#     )
+#     net = cv2.dnn.readNetFromCaffe(PROTOTXT, MODEL)
+#     net.setInput(blob)
+#     detections = net.forward()
+#     return detections
 
-        if confidence > confidence_threshold:
-            # extract the index of the class label from the `detections`,
-            # then compute the (x, y)-coordinates of the bounding box for
-            # the object
-            idx = int(detections[0, 0, i, 1])
-            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-            (startX, startY, endX, endY) = box.astype("int")
+# @st.cache
+# def annotate_image(
+#     image, detections, confidence_threshold=DEFAULT_CONFIDENCE_THRESHOLD
+# ):
+#     # loop over the detections
+#     (h, w) = image.shape[:2]
+#     labels = []
+#     for i in np.arange(0, detections.shape[2]):
+#         confidence = detections[0, 0, i, 2]
 
-            # display the prediction
-            label = f"{CLASSES[idx]}: {round(confidence * 100, 2)}%"
-            labels.append(label)
-            cv2.rectangle(image, (startX, startY), (endX, endY), COLORS[idx], 2)
-            y = startY - 15 if startY - 15 > 15 else startY + 15
-            cv2.putText(
-                image, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2
-            )
-    return image, labels
+#         if confidence > confidence_threshold:
+#             # extract the index of the class label from the `detections`,
+#             # then compute the (x, y)-coordinates of the bounding box for
+#             # the object
+#             idx = int(detections[0, 0, i, 1])
+#             box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+#             (startX, startY, endX, endY) = box.astype("int")
 
-# st.title("Object detection with MobileNet SSD")
-# img_file_buffer = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
-# confidence_threshold = st.slider(
-#     "Confidence threshold", 0.0, 1.0, DEFAULT_CONFIDENCE_THRESHOLD, 0.05
-# )
+#             # display the prediction
+#             label = f"{CLASSES[idx]}: {round(confidence * 100, 2)}%"
+#             labels.append(label)
+#             cv2.rectangle(image, (startX, startY), (endX, endY), COLORS[idx], 2)
+#             y = startY - 15 if startY - 15 > 15 else startY + 15
+#             cv2.putText(
+#                 image, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2
+#             )
+#     return image, labels
 
-# if img_file_buffer is not None:
-#     image = np.array(Image.open(img_file_buffer))
+# # st.title("Object detection with MobileNet SSD")
+# # img_file_buffer = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+# # confidence_threshold = st.slider(
+# #     "Confidence threshold", 0.0, 1.0, DEFAULT_CONFIDENCE_THRESHOLD, 0.05
+# # )
 
-# else:
-#     demo_image = DEMO_IMAGE
-#     image = np.array(Image.open(demo_image))
+# # if img_file_buffer is not None:
+# #     image = np.array(Image.open(img_file_buffer))
 
-# detections = process_image(image)
-# image, labels = annotate_image(image, detections, confidence_threshold)
+# # else:
+# #     demo_image = DEMO_IMAGE
+# #     image = np.array(Image.open(demo_image))
 
-# st.image(
-#     image, caption=f"Processed image", use_column_width=True,
-# )
+# # detections = process_image(image)
+# # image, labels = annotate_image(image, detections, confidence_threshold)
 
-# st.write(labels)
+# # st.image(
+# #     image, caption=f"Processed image", use_column_width=True,
+# # )
 
-# コールバック
-def callback(frame):
-    img = frame.to_ndarray(format="bgr24")
+# # st.write(labels)
 
-    detections = process_image(img)
-    # img, labels = annotate_image(img, detections, confidence_threshold)
-    # img, labels = annotate_image(img, detections, 0.5)
-    img, labels = annotate_image(img, detections, 0.1)
+# # コールバック
+# def callback(frame):
+#     img = frame.to_ndarray(format="bgr24")
 
-    return av.VideoFrame.from_ndarray(img, format="bgr24")
+#     detections = process_image(img)
+#     # img, labels = annotate_image(img, detections, confidence_threshold)
+#     # img, labels = annotate_image(img, detections, 0.5)
+#     img, labels = annotate_image(img, detections, 0.1)
 
-# メイン
-def appmain():
-    st.title("みまもりくん")
-    st.subheader('＜物体検知＞')
+#     return av.VideoFrame.from_ndarray(img, format="bgr24")
 
-    # ファイルダウンロード
-    download_file(MODEL_URL, MODEL_LOCAL_PATH, expected_size=23147564) # 学習モデル
-    download_file(PROTOTXT_URL, PROTOTXT_LOCAL_PATH, expected_size=29353) # プロトコル
+# # メイン
+# def appmain():
+#     st.title("みまもりくん")
+#     st.subheader('＜物体検知＞')
 
-    webrtc_streamer(
-        key="video",
-        video_frame_callback=callback,
-        rtc_configuration={
-            "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
-        }
-    )
+#     # ファイルダウンロード
+#     download_file(MODEL_URL, MODEL_LOCAL_PATH, expected_size=23147564) # 学習モデル
+#     download_file(PROTOTXT_URL, PROTOTXT_LOCAL_PATH, expected_size=29353) # プロトコル
+
+#     webrtc_streamer(
+#         key="video",
+#         video_frame_callback=callback,
+#         rtc_configuration={
+#             "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+#         }
+#     )
 
 #コマンドプロンプト上で表示する
 if __name__ == "__main__":
     #関数を呼び出す
-    appmain()
+    appmain(Object_ID_object)
